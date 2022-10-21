3e440fb8b5d4fbbb0ddf78d93647b735f2ead8d8
devmanoshri <manoshri.no@gmail.com>
2022-10-21 11:46:05 +0200

    conditional run by useEffect

--- src/components/UseEffectHook.js
+++ src/components/UseEffectHook.js
@@ -2,12 +2,19 @@ import React, { useState, useEffect } from "react";
 
 export default function UseEffectHook() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
   useEffect(() => {
+    console.log("useEffect - updated");
     document.title = `You Clicked ${count} times`;
-  });
+  }, [count]);
 
   return (
     <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={() => setCount(count + 1)}>Click {count} times</button>
     </div>
   );
