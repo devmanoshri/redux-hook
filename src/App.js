5a1959e64a640b04217f44925cdba3e1dddd648d
devmanoshri <manoshri.no@gmail.com>
2022-10-22 16:15:14 +0200

    UseEffect Data Fetching

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
+
 //import UseStateHook from "./components/UseStateHook";
 //import UseEffectHook from "./components/UseEffectHook";
 //import MouseContainer from "./components/MouseContainer";
-import IntervalHookCounter from "./components/IntervalHookCounter";
+//import IntervalHookCounter from "./components/IntervalHookCounter";
+import UseEffectDataFetching from "./components/UseEffectDataFetching";
 function App() {
   return (
     <div className="App">
       {/* <UseStateHook /> */}
       {/* <UseEffectHook /> */}
       {/* <MouseContainer /> */}
-      <IntervalHookCounter />
+      {/* <IntervalHookCounter /> */}
+      <UseEffectDataFetching />
     </div>
   );
 }
